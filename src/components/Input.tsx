d5cea3e51375127fae65a0886baf4a88c2ef7717
Batty-sk <saorav.skumar@gmail.com>
2024-10-09 00:44:36 +0530

    fixes: create-nft ui

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,19 +15,21 @@ let jsxElement =<></>
 
 switch(inputType){
     case 'textarea':
-        jsxElement=<textarea rows={5} onChange={handleOnChange} className='w-full font-normal border-2 border-gray-300 rounded-md' placeholder={placeHolder}/>
+        jsxElement=<textarea rows={5} onChange={handleOnChange} className='w-full outline-none   font-poppins font-semibold  bg-slate-50' placeholder={placeHolder}/>
         break;
     case 'price':
-        jsxElement= <div className='flex items-center justify-center'><input onChange={handleOnChange} type={'number'} placeholder={placeHolder} className='w-full font-normal h-10 border-2 border-gray-300 rounded-md me-2'></input> <p>ETH</p> </div>
+        jsxElement= <div className='flex items-center justify-center'><input onChange={handleOnChange} type={'number'} placeholder={placeHolder} className='w-full h-10   font-poppins font-semibold outline-none me-2  bg-slate-50'></input> <p className='p-3'>ETH</p> </div>
         break;
     default:
-        jsxElement=<input onChange={handleOnChange} type={inputType} placeholder={placeHolder} className='w-full font-normal h-10 border-2 border-gray-300  rounded-md' />
+        jsxElement=<input onChange={handleOnChange} type={inputType} placeholder={placeHolder} className='w-full  h-10  outline-none   bg-slate-50 font-poppins font-semibold' />
 
 }
   return (
-    <div className='font-poppins font-semibold dark:text-white text-black'>
+    <div className='font-poppins font-semibold font-semibold dark:text-white text-black '>
         <p className='pb-2 pt-5 '>{title}</p>
+        <div className='bg-slate-100 shadow-md border'>
         {jsxElement}
+        </div>
     </div>
   )
 }
