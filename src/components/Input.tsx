4efda52809cd509c1c9e62b8ba0671f9e60030c7
Batty-sk <saorav.skumar@gmail.com>
2024-10-13 21:20:18 +0530

    fix: Ui/Ux of Create-nft page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,19 +15,19 @@ let jsxElement =<></>
 
 switch(inputType){
     case 'textarea':
-        jsxElement=<textarea rows={5} onChange={handleOnChange} className='w-full outline-none   font-poppins font-semibold  bg-slate-50' placeholder={placeHolder}/>
+        jsxElement=<textarea rows={5} onChange={handleOnChange} className='w-full outline-none   font-poppins bg-[#eff7f6]' placeholder={placeHolder}/>
         break;
     case 'price':
-        jsxElement= <div className='flex items-center justify-center'><input onChange={handleOnChange} type={'number'} placeholder={placeHolder} className='w-full h-10   font-poppins font-semibold outline-none me-2  bg-slate-50'></input> <p className='p-3'>ETH</p> </div>
+        jsxElement= <div className='flex items-center justify-center'><input onChange={handleOnChange} type={'number'} placeholder={placeHolder} className='w-full h-10   font-poppins  outline-none me-2  bg-[#eff7f6]'></input> <p className='p-3'>ETH</p> </div>
         break;
     default:
-        jsxElement=<input onChange={handleOnChange} type={inputType} placeholder={placeHolder} className='w-full  h-10  outline-none   bg-slate-50 font-poppins font-semibold' />
+        jsxElement=<input onChange={handleOnChange} type={inputType} placeholder={placeHolder} className='w-full  h-10  outline-none   bg-[#eff7f6] font-poppins ' />
 
 }
   return (
     <div className='font-poppins  font-semibold dark:text-white text-black '>
-        <p className='pb-2 pt-5 '>{title}</p>
-        <div className='bg-slate-100 shadow-sm border'>
+        <p className='pb-1 pt-5 '>{title}</p>
+        <div className='bg-[#eff7f6] shadow-sm border font-normal text-sm'>
         {jsxElement}
         </div>
     </div>
--- src/pages/CreateNFT.tsx
+++ src/pages/CreateNFT.tsx
@@ -2,42 +2,87 @@ import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { useState } from "react";
 
 import { Link } from "react-router-dom";
-import { Input,Button } from "../components";
-
+import { Input, Button } from "../components";
 
 const CreateNFT = () => {
-
-    const [formFields,updateFormFields]=useState({title:'',price:0.0,description:''})
+  const [formFields, updateFormFields] = useState({
+    title: "",
+    price: 0.0,
+    description: "",
+  });
   return (
     <main className="p-10 dark:bg-black bg-white  w-full flex flex-col justify-center items-center mb-10">
-        <div className="flex flex-col justify-center items-center md:w-3/5 w-full">
-      <div className="flex justify-start w-full">
-        <h1 className="font-poppins dark:text-white text-black text-2xl font-semibold">
-          Create NFT
-        </h1>
-      </div>
-
-      <div className="flex flex-col justify-center items-center w-full   py-14 px-8 rounded-lg ">
-        <div className="w-full  bg-slate-100 shadow-md rounded-md flex justify-center p-8">
-        <div className="upload-image w-8/12 border-2 border-gray-500 border-dotted dark:border-white flex py-8 px-8 flex-col items-center rounded-lg">
-            <p className="font-semibold font-poppins dark:text-white text-black">Upload: SVG, PNG, JPG </p>
-            <AddPhotoAlternateIcon  style={{fontSize:170,color:''}}/>
-            <p className="font-semibold font-poppins dark:text-white text-black">Drag & Drop Or </p>
-            <p><Link to="" className="underline font-semibold text-sm"> Browse A File From Your Computer </Link></p>
-        </div>
+      <div className="flex flex-col justify-center items-center md:w-3/5 w-full">
+        <div className="flex justify-start w-full">
+          <h1 className="font-poppins dark:text-white text-black text-2xl font-semibold">
+            Create NFT
+          </h1>
         </div>
 
-        <div className="mt-4 w-full">
-            <Input title="Title" inputType="text" handleOnChange={(e)=>{updateFormFields(prev=>({...prev,title:e.target.value}))}}  placeHolder="Title"/>
-            <Input title="Description" inputType="textarea" handleOnChange={(e)=>{updateFormFields(prev=>({...prev,description:e.target.value}))}}  placeHolder="Write something about ur NFT ..."/>
-            <Input title="Price" inputType="price" handleOnChange={(e)=>{updateFormFields(prev=>({...prev,price:+(e.target.value)}))}}  placeHolder="Price"/>
+        <div className="flex flex-col justify-center items-center w-full py-16 px-8 ">
+          <div className="w-full  bg-[#eff7f6]  rounded-sm flex justify-center ">
+            <div className="upload-image w-full border-2 border-gray-500 border-dotted dark:border-white flex py-8 px-8 flex-col items-center rounded-lg">
+              <p className="font-semibold font-poppins dark:text-white text-black">
+                Upload: SVG, PNG, JPG{" "}
+              </p>
+              <AddPhotoAlternateIcon style={{ fontSize: 170, color: "" }} />
+              <p className="font-semibold font-poppins dark:text-white text-black">
+                Drag & Drop Or{" "}
+              </p>
+              <p>
+                <Link to="" className="underline font-semibold text-sm">
+                  {" "}
+                  Browse A File From Your Computer{" "}
+                </Link>
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-4 w-full">
+            <Input
+              title="Title"
+              inputType="text"
+              handleOnChange={(e) => {
+                updateFormFields((prev) => ({
+                  ...prev,
+                  title: e.target.value,
+                }));
+              }}
+              placeHolder="Title"
+            />
+            <Input
+              title="Description"
+              inputType="textarea"
+              handleOnChange={(e) => {
+                updateFormFields((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }));
+              }}
+              placeHolder="Description ..."
+            />
+            <Input
+              title="Price"
+              inputType="price"
+              handleOnChange={(e) => {
+                updateFormFields((prev) => ({
+                  ...prev,
+                  price: +e.target.value,
+                }));
+              }}
+              placeHolder="Price"
+            />
             <div className="flex mt-8">
-              <Button title="Create" path="/" handleOnClickOrChange={()=>{}} />
-            </div> 
-        </div>
+              <Button
+                title="Create"
+                path="/"
+                handleOnClickOrChange={() => {}}
+              />
+            </div>
+          </div>
 
-        <div></div>
-      </div>
+          <div></div>
+        </div>
       </div>
     </main>
   );
