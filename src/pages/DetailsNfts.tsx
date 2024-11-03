7cdf0e0fd1644fd00a1b7d80b8a0bc57f226c1d5
Batty-sk <saorav.skumar@gmail.com>
2024-11-03 11:14:28 +0530

    added: more nfts features in DetailsNFTs page

--- src/pages/DetailsNfts.tsx
+++ src/pages/DetailsNfts.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { images } from "../assets";
+import { ShoppingCart } from "@mui/icons-material";
+import AppsIcon from '@mui/icons-material/Apps';
 
 const DetailsNfts = () => {
+  const nftId= useParams()  // this will also returns the from which page its been called from 
+  // eg - if nft clicked from market then show the more nfts from market nfts, if listed nfts then show the 
+  // listed nfts , if bought nfts then show the other bought nfts in More NFTs section.
+  // or 
+  // just get the more nft items from the same owner...
+  useEffect(()=>{
+    
+      //fetch the nftId
+
+    return()=>{
+
+    }
+  })
   return (
     <section className="flex justify-center">
-      <div className="flex flex-col justify-center items-center md:w-3/5">
-        <div className="flex p-8 bg-gray-100 mt-10 rounded-tl-3xl mb-8">
+      <div className="flex flex-col justify-center items-center md:w-4/5">
+        <div className="flex p-8 bg-gray-100 shadow-sm shadow-gray-400  mt-10 rounded-tl-3xl mb-8">
           <img
             src={images.nfts[0]}
             height={250}
-            width={200}
+            width={280}
             alt="nft"
             style={{ objectFit: "cover" }}
             className="rounded-tl-3xl"
@@ -32,10 +49,14 @@ const DetailsNfts = () => {
             </p>
             </div>
             <div className="mt-3">
-                <button className="px-3 py-2 rounded-md shadow-md hover:scale-105 transition-all shadow-black bg-green-700 font-poppins text-white">Buy Now</button>
+                <button className="px-3 py-2 rounded-md shadow-md hover:scale-105 transition-all shadow-black bg-green-700 font-poppins text-white"><ShoppingCart className="pr-2"/>Buy Now</button>
             </div>
           </div>
 
+
+        </div> 
+        <div className="flex flex-col">
+            <h1 className="text-3xl font-semibold flex items-center font-poppins "><AppsIcon className="pr-3" fontSize="large"/>More From This Owner</h1>
         </div>
       </div>
     </section>
